9c6dd0b5bcacb637275afbc367621d5f8aea0d36
JTStephens18 <JTStephens18@gmail.com>
2023-02-05 13:26:49 -0600

    <feat>[main]: Added uvicorn start

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
-from fastapi import FastAPI
-import imageGenerator as ig
-from fastapi.responses import FileResponse
-from fastapi.responses import Response
+import uvicorn
 
-app = FastAPI()
-
-
-@app.get("/test")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/images/{tokenId}/{background}/{head}/{eyes}/{mouth}/{accessory}/{weapon}")
-async def test(tokenId: int, background: str, head: str, eyes: str, mouth: str, accessory: str, weapon: str):
-    testing = ig.generateImage(
-        tokenId, background, head, eyes, mouth, accessory, weapon)
-    # return Response(content=testing, media_type="image/png")
-    return FileResponse(f'{tokenId}.png', media_type='image/png')
-    return {"msg": f"{testing}"}
-    # return {"message": f"{background} {head} {eyes} {mouth} {accessory} {weapon}"}
+if __name__ == "__main__":
+    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
